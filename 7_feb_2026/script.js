8b070600eaca588a369dd9205ae6517580dffbfc
mamathauppalwai8 <mamathauppalwai8@gmail.com>
2026-02-09 18:41:48 +0530

    Add files via upload

---
+++ 7_feb_2026/script.js
@@ -0,0 +1,7 @@
+document.getElementById("primaryCTA").addEventListener("mouseover", () => {
+  document.body.style.backgroundColor = "#f7faff";
+});
+
+document.getElementById("primaryCTA").addEventListener("mouseout", () => {
+  document.body.style.backgroundColor = "white";
+});
